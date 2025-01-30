e4a44b937bb29411e22eb72fb0379cfbc16aed91
Colin Beveridge <colin@flyingcoloursmaths.co.uk>
2025-01-30 12:25:23 +0000

    Update alfabet2.py to produce svg

--- alfabet2.py
+++ alfabet2.py
@@ -15,9 +15,9 @@ paths = []
 # a path pair is two paths with centre1 < centre2 
 for p in itertools.permutations(points, 6):
     if p[0] > p[3]: # throw out bad centres
-        continue 
+        continue
     if p[1] > p[2] or p[4] > p[5]: # throw out bad node pairs
-        continue 
+        continue
     if not(can_connect(p[0], p[1])) or not(can_connect(p[0], p[2])):
         continue # bin disconnected first pair
     if not (can_connect(p[3], p[4])) or not (can_connect(p[3],p[5])):
@@ -53,7 +53,7 @@ def print_path(p):
         elif abs(c1-c2) == 3:
             char = "-"
         elif abs(c1-c2) == 4:
-            char = "\\" 
+            char = "\\"
         grid[x][y] = char
     for g in grid:
         out += ( "".join(g)) + "\n"
@@ -62,3 +62,44 @@ def print_path(p):
 for i, p in enumerate(paths):
     print(i)
     print(print_path(p))
+
+WIDTH = 500
+HEIGHT = 500
+
+def xy(n, k ):
+    xx = k % 24
+    yy = k // 24
+    return (n %3 + 1) * WIDTH/4 + xx* WIDTH, (n // 3 + 1) * HEIGHT / 4 + yy * HEIGHT
+
+
+def generate_svg(path, k):
+    out = ""
+    # circles and rects, let's not overcomplicate
+    for p in range(9):
+        x, y = xy(p, k)
+        if p in path:
+            out += f'<circle fill = "black" cx="{x}" cy="{y}" r="{WIDTH/20}"/>\n'
+        else:
+            # out += f'<circle fill = "black" cx="{x}" cy="{y}" r="{WIDTH/50}"/>\n'
+            pass
+    for i in {0,3}:
+        pts = ""
+        for j in [1+i, 2+i]:
+            x,y = xy(path[j], k)
+            X,Y = xy(path[i], k)
+            pts = f" {x},{y} {X},{Y}"
+            out += f'<polyline stroke="black" points = "{pts}" stroke-width="{WIDTH/50}"/>\n'
+    return out
+
+def full_svg(paths):
+    out = '<svg xmlns="http://www.w3.org/2000/svg" width="12000" height="12000" viewBox="0 0 12000 12000" fill="none">\n'
+    for k,p in enumerate(paths):
+        out += generate_svg(p, k)
+    out += "</svg>"
+    return out
+
+# with open("glyphs.svg", "w") as f:
+#     f.write(full_svg(paths))
+
+print(generate_svg((0,1,4,5,7,8),0))
+print(generate_svg((1,3,4,7,5,8),0))
